7fc95285bd9942079a0340ebaba5656042357a0e
bahareh.Smi <bahareh_smaili_1990@yahoo.com>
2023-04-20 16:00:25 +0330

    Add files via upload

---
+++ NewCsharp11/ClassINumberInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCsharp11
+{
+    internal class ClassINumberInterface
+    {
+        public void testINumberInterface()
+        {
+            var ints = new int[] { 1, 2, 3, 4, 5 };
+            var doubles = new double[] { 1.3, 2.8, 3.5, 4.2, 5.1 };
+
+            Console.WriteLine(Sum(ints));
+            Console.WriteLine(Sum(doubles));
+
+            Console.WriteLine(Avg(ints));
+            Console.WriteLine(Avg(doubles));
+        }
+
+        T Sum<T>(T[] values) where T : INumber<T>
+        {
+            T result = T.Zero;
+            foreach (T value in values)
+            {
+                result += value;
+            }
+            return result;
+        }
+
+        T Avg<T>(T[] values) where T : INumber<T>
+        {
+            var sum = Sum(values);
+            T count = T.Zero;
+            for (int i = 0; i < values.Length; i++)
+            {
+                count += T.One;
+            }
+            return sum / count;
+
+        }
+    }
+}
---
+++ NewCsharp11/Program.cs
@@ -0,0 +1,12 @@
+﻿using NewCsharp11;
+using System.Numerics;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Created by : Bahareh.Smi");
+        Console.WriteLine("Date : 1402/01/31");
+        
+        new ClassINumberInterface().testINumberInterface();
+    }
+}
